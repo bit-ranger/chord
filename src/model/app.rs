066d8dd95d08351b9ce4e3cbeb5ace4d3a56be50
bit-ranger <sincerebravefight@gmail.com>
2021-03-05 22:40:46 +0800

    app context

--- src/model/app.rs
+++ src/model/app.rs
@@ -1,27 +1,37 @@
-use handlebars::Handlebars;
-
+use handlebars::{Handlebars};
+use std::borrow::Borrow;
 
 pub trait AppContext{
 
 
-    fn get_render(&self) -> &fn ();
+    fn get_handlebars<T>(&self) -> &Handlebars;
 }
 
-// #[derive(Debug)]
-// pub struct AppContextStruct<'reg> {
-//
-// }
+#[derive(Debug)]
+pub struct AppContextStruct<'reg> {
 
-// impl AppContextStruct {
-//     const handlebars: Handlebars<'_> = Handlebars::new();
-// }
+    handlebars: Handlebars<'reg>
+}
 
-// impl AppContext for AppContextStruct{
-//
-//
-//
-//     fn get_render(&self) -> &fn() {
-//         Handlebars::render_template;
-//     }
-// }
+impl <'reg> AppContextStruct<'reg> {
+
+    pub fn new() -> AppContextStruct<'reg>{
+        AppContextStruct{
+            handlebars: Handlebars::new()
+        }
+    }
+
+}
+
+impl <'reg> AppContext for AppContextStruct <'reg>{
+
+
+
+    fn get_handlebars<T>(self: &AppContextStruct<'reg>) -> &Handlebars<'reg>
+    {
+        self.handlebars.borrow()
+    }
+
+
+}
 

5e5b90c11ae4c3c1024da49dfda060bf8535b67b
bit-ranger <sincerebravefight@gmail.com>
2021-03-06 22:10:49 +0800

    feat(restapi): error

--- src/point/restapi.rs
+++ src/point/restapi.rs
@@ -2,53 +2,66 @@ use crate::model::{Error, Json, PointResult, PointContext};
 use surf::{RequestBuilder, Response, Url};
 use surf::http::Method;
 use std::str::FromStr;
-use std::collections::HashMap;
-use serde_json::{Value, Map, Number};
 use surf::http::headers::{HeaderName, HeaderValue};
-use futures::TryFutureExt;
-use std::borrow::BorrowMut;
+use serde_json::Map;
 
 pub async fn run(context: &dyn PointContext) -> PointResult{
-    let url = context.get_config_rendered(vec!["url"]).unwrap();
-    println!("url {}", url);
+    let url = match context.get_config_rendered(vec!["url"]){
+        Some(url) => url,
+        None => return Err(Error::new("010", "missing url"))
+    };
 
-    let method = context.get_config_rendered(vec!["method"]).unwrap();
-    let mut rb = RequestBuilder::new(Method::from_str(method.as_str()).unwrap(),
-                                     Url::from_str(url.as_str()).unwrap());
+    let url =match Url::from_str(url.as_str()){
+        Ok(url) => url,
+        Err(_) => return Err(Error::new("011", "invalid url"))
+    };
+
+    let method = match context.get_config_rendered(vec!["method"]){
+        Some(method) => method,
+        None => return Err(Error::new("020", "missing method"))
+    };
+
+    let method = match Method::from_str(method.as_str()){
+        Ok(method) => method,
+        Err(_) => return Err(Error::new("021", "invalid method"))
+    };
+
+    let mut rb = RequestBuilder::new(method, url);
 
     if let Some(header) = context.get_config()["header"].as_object() {
         for (k,v) in header.iter() {
-            rb = rb.header(HeaderName::from_str(context.render(k).as_str()).unwrap(),
-                      HeaderValue::from_str(context.render(v.as_str().unwrap()).as_str()).unwrap());
+            let hn = HeaderName::from_str(context.render(k).as_str());
+            if hn.is_err(){
+                return Err(Error::new("030","invalid header name"));
+            }
+            let hv= HeaderValue::from_str(context.render(v.as_str().unwrap()).as_str());
+            if hv.is_err(){
+                return Err(Error::new("031","invalid header value"));
+            }
+            rb = rb.header(hn.unwrap(), hv.unwrap());
         }
     }
 
-    let mut response: surf::Result<Response> = rb.send().await;
-    match response {
+    let  response: surf::Result<Response> = rb.send().await;
+    return match response {
         Ok(mut res) => {
             let mut res_data = Map::<String, Json>::new();
             res_data.insert(String::from("status"), Json::String(String::from(res.status().to_string())));
 
-            let mut header_data:Map<String,Json> = Map::new();
+            let mut header_data: Map<String, Json> = Map::new();
             for header_name in res.header_names() {
                 header_data.insert(header_name.to_string(), Json::String(res.header(header_name).unwrap().to_string()));
             }
 
             res_data.insert(String::from("header"), Json::Object(header_data));
 
-            let body:Json = res.body_json().await.unwrap();
+            let body: Json = res.body_json().await.unwrap();
             res_data.insert(String::from("body"), body);
-            return Ok(Json::Object(res_data));
+            Ok(Json::Object(res_data))
         },
         Err(e) => {
-            return Err(Error::new("000", "response is not a json"));
+            Err(Error::new("000", format!("http error: {}", e).as_str()))
         }
     }
-
-
-    let mut res_data:HashMap<String, Value> = HashMap::new();
-
-
-
 
 }

983f25efda1620da633599f37d7a46384db8995b
bit-ranger <sincerebravefight@gmail.com>
2021-03-10 10:36:42 +0800

    feat(main): desc

--- src/model/error.rs
+++ src/model/error.rs
@@ -6,8 +6,7 @@ pub type Error = ErrorStruct;
 #[derive(Debug)]
 pub struct ErrorStruct{
     code: String,
-    message: String,
-    desc: String
+    message: String
 }
 
 impl ErrorStruct{
@@ -15,8 +14,7 @@ impl ErrorStruct{
     pub fn new(code: &str, message: &str) -> ErrorStruct{
         ErrorStruct{
             code: String::from(code),
-            message: String::from(message),
-            desc: format!("{} \"code\": \"{}\", \"message\": \"{}\" {}", "{", code, message, "}")
+            message: String::from(message)
         }
     }
 
@@ -32,13 +30,14 @@ impl ErrorStruct{
 
 impl Display for ErrorStruct {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        f.write_str(&self.desc)
+        f.write_fmt(format_args!("{} \"code\": \"{}\", \"message\": \"{}\" {}",
+                                 "{", self.code, self.message, "}"))
     }
 }
 
 impl std::error::Error for ErrorStruct {
     fn description(&self) -> &str {
-        &self.desc
+        &self.code
     }
 
     fn cause(self: &ErrorStruct) -> Option<&dyn std::error::Error> {

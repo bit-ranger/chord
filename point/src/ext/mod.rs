be41a5e42ff59f9de3d11c9f58d4b263d937f7b7
bit-ranger <sincerebravefight@gmail.com>
2021-03-20 20:30:23 +0800

    feat(point): sleep

--- point/src/ext/mod.rs
+++ point/src/ext/mod.rs
@@ -1,15 +1,5 @@
-use common::point::PointArg;
-use std::str::FromStr;
-
 pub mod restapi;
 pub mod md5;
 pub mod dubbo;
 pub mod sleep;
 
-
-fn config_rendered_default<T>(point_arg: &dyn PointArg, path: Vec<&str>, default: T) -> Result<T, T::Err> where T: FromStr {
-    match point_arg.config_rendered(path){
-        Some(s) => s.parse(),
-        None=> Ok(default)
-    }
-}
--- point/src/ext/sleep.rs
+++ point/src/ext/sleep.rs
@@ -3,10 +3,9 @@ use common::value::Json;
 use crate::model::PointValue;
 use async_std::task::sleep;
 use std::time::Duration;
-use crate::ext::config_rendered_default;
 
 pub async fn run(point_arg: &dyn PointArg) -> PointValue {
-    let seconds = config_rendered_default(point_arg, vec!["seconds"], 0)?;
+    let seconds = point_arg.config()["seconds"].as_i64().unwrap_or(0) as u64;
     sleep(Duration::from_secs(seconds)).await;
     return Ok(Json::Null);
 }

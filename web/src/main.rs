778901eda4c0d3f1af460927f4ee231dc4261d70
bit-ranger <sincerebravefight@gmail.com>
2021-04-12 22:46:32 +0800

    feat(web): validation

--- web/src/main.rs
+++ web/src/main.rs
@@ -1,10 +1,10 @@
 mod controller;
-use controller::job::exec as job__exec;
 use tide::{Request, Response};
 use chord_common::value::Json;
 use chord_common::error::Error;
 use tide::prelude::*;
 use tide::http::StatusCode;
+use validator::{ValidationErrors, ValidationErrorsKind};
 
 #[derive(Serialize, Deserialize)]
 struct ErrorBody{
@@ -19,19 +19,52 @@ fn common_error_json(e: &Error) -> Json {
             })
 }
 
-// fn validator_error_json(e: &ValidationErrors) -> Json {
-//     json!(ErrorBody{
-//                 code: "400".into(),
-//                 message: "illegal argument".into()
-//             })
-// }
+fn validator_error_json_nested(e: &ValidationErrors) -> Vec<String> {
+    return e.errors()
+        .iter()
+        .map(|(k, e)|
+            match e {
+                ValidationErrorsKind::Field(ev) =>
+                    ev.iter()
+                        .map(|e| format!("[{}] {}", k, e.to_string()))
+                        .collect(),
+                ValidationErrorsKind::Struct(f) =>
+                    validator_error_json_nested(f.as_ref()),
+                ValidationErrorsKind::List(m) =>
+                    m.iter()
+                        .map(|(_i, e)|
+                            validator_error_json_nested(e.as_ref()))
+                        .fold(Vec::new(), |mut l,e| {
+                            l.extend(e);
+                            return l;
+                        })
+            }
+        )
+        .fold(Vec::new(), |mut l,e| {
+            l.extend(e);
+            return l;
+        })
+}
+
+fn validator_error_json(e: &ValidationErrors) ->Json{
+    json!(ErrorBody{
+                code: "400".into(),
+                message: validator_error_json_nested(e).into_iter().last().unwrap()
+            })
+}
+
+
 
 
 #[macro_export]
 macro_rules! json_handler {
-    ($func:ident) => {{
+    ($func:path) => {{
         |mut req: Request<()>| async move {
             let rb =  req.body_json().await?;
+            if let Err(e) = validator::Validate::validate(&rb){
+                return Ok(Response::builder(StatusCode::InternalServerError)
+                    .body(validator_error_json(&e)))
+            };
             let rst = $func(rb).await;
             match rst{
                 Ok(r) => Ok(Response::builder(StatusCode::Ok)
@@ -48,7 +81,7 @@ async fn main() -> tide::Result<()> {
     let mut app = tide::new();
 
     app.at("/job/exec").post(
-        json_handler!(job__exec)
+        json_handler!(controller::job::exec)
     );
 
     app.listen("127.0.0.1:8080").await?;

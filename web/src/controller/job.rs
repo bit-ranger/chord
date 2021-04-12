360a836e17bfa6b95d6b55116cd49ec148c87a73
bit-ranger <sincerebravefight@gmail.com>
2021-04-13 00:35:14 +0800

    feat(web): job

--- web/src/controller/job.rs
+++ web/src/controller/job.rs
@@ -12,10 +12,10 @@ pub struct Job {
     name: String
 }
 
-pub async fn exec(job: Job) -> Result<Job, Error> {
+pub async fn exec(job: Job) -> Result<String, Error> {
     let execution_id = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_millis().to_string();
     let app_ctx = chord_flow::create_app_context(Box::new(PointRunnerDefault::new())).await;
     let job_path = Path::new("/data/chord").join(&job.name);
     let _task_state_vec = crate::job::run(job_path, execution_id.as_str(), app_ctx).await;
-    return Ok(job);
+    return Ok(execution_id);
 }

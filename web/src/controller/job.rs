cb93d84340b365abc53e9b74e0d834dcfd23966d
bit-ranger <sincerebravefight@gmail.com>
2021-04-13 11:30:54 +0800

    feat(web): refactor

--- web/src/controller/job.rs
+++ web/src/controller/job.rs
@@ -4,6 +4,7 @@ use validator::{Validate};
 use std::time::SystemTime;
 use async_std::path::Path;
 use chord_point::PointRunnerDefault;
+use crate::service;
 
 
 #[derive(Debug, Serialize, Deserialize, Validate)]
@@ -16,6 +17,6 @@ pub async fn exec(job: Job) -> Result<String, Error> {
     let execution_id = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_millis().to_string();
     let app_ctx = chord_flow::create_app_context(Box::new(PointRunnerDefault::new())).await;
     let job_path = Path::new("/data/chord").join(&job.name);
-    let _task_state_vec = crate::job::run(job_path, execution_id.as_str(), app_ctx).await;
+    let _task_state_vec = service::job::run(job_path, execution_id.as_str(), app_ctx).await;
     return Ok(execution_id);
 }
---
+++ web/src/framework/mod.rs
@@ -0,0 +1,95 @@
+use std::path::Path;
+
+use tide::{Request, Response};
+use tide::http::StatusCode;
+use tide::prelude::*;
+use validator::{ValidationErrors, ValidationErrorsKind};
+
+use chord_common::error::Error;
+use chord_common::value::Json;
+
+mod logger;
+use crate::controller;
+
+#[derive(Serialize, Deserialize)]
+struct ErrorBody {
+    code: String,
+    message: String,
+}
+
+fn common_error_json(e: &Error) -> Json {
+    json!(ErrorBody{
+                code: e.code().into(),
+                message: e.message().into()
+            })
+}
+
+fn validator_error_json_nested(e: &ValidationErrors) -> Vec<String> {
+    return e.errors()
+        .iter()
+        .map(|(k, e)|
+            match e {
+                ValidationErrorsKind::Field(ev) =>
+                    ev.iter()
+                        .map(|e| format!("[{}] {}", k, e.to_string()))
+                        .collect(),
+                ValidationErrorsKind::Struct(f) =>
+                    validator_error_json_nested(f.as_ref()),
+                ValidationErrorsKind::List(m) =>
+                    m.iter()
+                        .map(|(_i, e)|
+                            validator_error_json_nested(e.as_ref()))
+                        .fold(Vec::new(), |mut l, e| {
+                            l.extend(e);
+                            return l;
+                        })
+            }
+        )
+        .fold(Vec::new(), |mut l, e| {
+            l.extend(e);
+            return l;
+        });
+}
+
+fn validator_error_json(e: &ValidationErrors) -> Json {
+    json!(ErrorBody{
+                code: "400".into(),
+                message: validator_error_json_nested(e).into_iter().last().unwrap()
+            })
+}
+
+
+#[macro_export]
+macro_rules! json_handler {
+    ($func:path) => {{
+        |mut req: Request<()>| async move {
+            let rb =  req.body_json().await?;
+            if let Err(e) = validator::Validate::validate(&rb){
+                return Ok(Response::builder(StatusCode::InternalServerError)
+                    .body(validator_error_json(&e)))
+            };
+            let rst = $func(rb).await;
+            match rst{
+                Ok(r) => Ok(Response::builder(StatusCode::Ok)
+                    .body(json!(r))),
+                Err(e) => Ok(Response::builder(StatusCode::InternalServerError)
+                    .body(common_error_json(&e)))
+            }
+        }
+    }}
+}
+
+
+pub async fn init() -> Result<(), Error>{
+    let mut app = tide::new();
+
+    let log_file_path = Path::new("/data/logs/chord/log.log");
+    let _log_handler = logger::init(vec![], &log_file_path).await?;
+
+    app.at("/job/exec").post(
+        json_handler!(controller::job::exec)
+    );
+
+    app.listen("127.0.0.1:8080").await?;
+    Ok(())
+}

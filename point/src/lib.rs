405995c5bb9faa292dcdbf5f43e880eb3c8ba719
bit-ranger <sincerebravefight@gmail.com>
2021-04-20 23:29:52 +0800

    refactor(point): cfg

--- point/src/lib.rs
+++ point/src/lib.rs
@@ -1,11 +1,10 @@
 use std::future::Future;
 use std::pin::Pin;
 
+use chord_common::err;
 use chord_common::point::{PointArg, PointRunner};
 
 mod point;
-
-use chord_common::{err};
 
 pub struct PointRunnerDefault;
 
@@ -18,7 +17,7 @@ impl PointRunnerDefault{
 impl PointRunner for PointRunnerDefault {
 
     fn run<'a>(&self, kind: &'a str, arg: &'a dyn PointArg) -> Pin<Box<dyn Future<Output=chord_common::point::PointValue> + Send  + 'a>> {
-        Box::pin(crate::run_point_kind(kind, arg))
+        Box::pin(point::run_point_kind(kind, arg))
     }
 }
 
@@ -29,27 +28,6 @@ unsafe impl Send for PointRunnerDefault
 
 unsafe impl Sync for PointRunnerDefault
 {
-}
-
-
-async fn run_point_kind(kind: &str, arg: &dyn PointArg) -> chord_common::point::PointValue{
-
-    let value = match kind.trim() {
-        "sleep" => point::sleep::run(arg).await,
-        #[cfg(feature = "pt_restapi")]
-        "restapi" => point::restapi::run(arg).await,
-        #[cfg(feature = "pt_md5")]
-        "md5" => point::md5::run(arg).await,
-        #[cfg(feature = "pt_dubbo")]
-        "dubbo" => point::dubbo::run(arg).await,
-        #[cfg(feature = "pt_mysql")]
-        "mysql" => point::mysql::run(arg).await,
-        #[cfg(feature = "pt_redis")]
-        "redis" => point::redis::run(arg).await,
-        _ => err!("002", format!("unsupported point kind {}", kind).as_str())
-    };
-
-    return value;
 }
 
 
--- point/src/point/mod.rs
+++ point/src/point/mod.rs
@@ -1,11 +1,39 @@
+use chord_common::point::PointArg;
+
+use crate::point;
+
+pub mod sleep;
+
 #[cfg(feature = "pt_restapi")]
 pub mod restapi;
 #[cfg(feature = "pt_md5")]
 pub mod md5;
 #[cfg(feature = "pt_dubbo")]
 pub mod dubbo;
-pub mod sleep;
 #[cfg(feature = "pt_mysql")]
 pub mod mysql;
 #[cfg(feature = "pt_redis")]
 pub mod redis;
+
+
+pub async fn run_point_kind(kind: &str, arg: &dyn PointArg) -> chord_common::point::PointValue{
+
+    let value = match kind.trim() {
+        "sleep" => sleep::run(arg).await,
+
+        #[cfg(feature = "pt_restapi")]
+        "restapi" => restapi::run(arg).await,
+        #[cfg(feature = "pt_md5")]
+        "md5" => md5::run(arg).await,
+        #[cfg(feature = "pt_dubbo")]
+        "dubbo" => dubbo::run(arg).await,
+        #[cfg(feature = "pt_mysql")]
+        "mysql" => mysql::run(arg).await,
+        #[cfg(feature = "pt_redis")]
+        "redis" => redis::run(arg).await,
+
+        _ => err!("002", format!("unsupported point kind {}", kind).as_str())
+    };
+
+    return value;
+}

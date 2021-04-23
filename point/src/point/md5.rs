3ba0650ec4f8614be24f361f3ef41be2bdfa8796
bit-ranger <sincerebravefight@gmail.com>
2021-04-23 15:21:27 +0800

    refactor(md5): rename

--- point/src/point/md5.rs
+++ point/src/point/md5.rs
@@ -1,8 +1,8 @@
 use chord_common::value::Json;
 use chord_common::point::{PointArg, PointValue};
 
-pub async fn run(context: &dyn PointArg) -> PointValue {
-    let raw = context.config_rendered(vec!["raw"]).unwrap();
+pub async fn run(pt_arg: &dyn PointArg) -> PointValue {
+    let raw = pt_arg.config_rendered(vec!["raw"]).unwrap();
     let digest = md5::compute(raw);
     let digest = format!("{:x}", digest);
     return Ok(Json::String(digest));

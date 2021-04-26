913b888561ba555a7d2a7bab24cd32eb2fd4e3c2
bit-ranger <sincerebravefight@gmail.com>
2021-04-26 22:39:28 +0800

    feat(web): thread pool async

--- web/src/controller/job.rs
+++ web/src/controller/job.rs
@@ -19,7 +19,8 @@ pub struct Req {
 pub struct Ctl {
     job_dir: PathBuf,
     work_dir: PathBuf,
-    app_ctx: Arc<dyn AppContext>
+    app_ctx: Arc<dyn AppContext>,
+    pool: rayon::ThreadPool
 }
 
 impl Ctl {
@@ -29,7 +30,8 @@ impl Ctl {
         Ctl {
             job_dir: Path::new(job_dir.as_str()).to_path_buf(),
             work_dir: Path::new(work_dir.as_str()).to_path_buf(),
-            app_ctx: block_on(chord_flow::create_app_context(Box::new(PointRunnerDefault::new())))
+            app_ctx: block_on(chord_flow::create_app_context(Box::new(PointRunnerDefault::new()))),
+            pool: rayon::ThreadPoolBuilder::new().build().unwrap()
         }
     }
 
@@ -38,8 +40,14 @@ impl Ctl {
         let exe_id = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_millis().to_string();
         let job_path = self.job_dir.clone().join(&req.name);
         let work_path = self.work_dir.clone().join(&req.name).join(exe_id.as_str());
-        let _ = async_std::fs::create_dir(work_path.clone()).await;
-        let _task_state_vec = service::job::run(job_path, work_path, exe_id.clone(), self.app_ctx.clone()).await;
+
+        let app_ctx_0 = self.app_ctx.clone();
+        let exe_id_0 = exe_id.clone();
+        self.pool.spawn(|| block_on(async {
+            let _ = async_std::fs::create_dir(work_path.clone()).await;
+            let _task_state_vec = service::job::run(job_path, work_path, exe_id_0, app_ctx_0).await;
+        }));
+
         return Ok(exe_id);
     }
 }

c1711a73b1febfb0d126725ef0481989ca5e98be
bit-ranger <sincerebravefight@gmail.com>
2021-05-07 17:44:39 +0800

    feat(point): feature rename

--- point/src/point/mod.rs
+++ point/src/point/mod.rs
@@ -4,17 +4,17 @@ use chord_common::error::Error;
 
 pub mod sleep;
 
-#[cfg(feature = "pt_jsonapi")]
+#[cfg(feature = "point_jsonapi")]
 pub mod jsonapi;
-#[cfg(feature = "pt_md5")]
+#[cfg(feature = "point_md5")]
 pub mod md5;
-#[cfg(feature = "pt_dubbo")]
+#[cfg(feature = "point_dubbo")]
 pub mod dubbo;
-#[cfg(feature = "pt_mysql")]
+#[cfg(feature = "point_mysql")]
 pub mod mysql;
-#[cfg(feature = "pt_redis")]
+#[cfg(feature = "point_redis")]
 pub mod redis;
-#[cfg(feature = "pt_mongodb")]
+#[cfg(feature = "point_mongodb")]
 pub mod mongodb;
 
 pub async fn create_kind_runner(kind: &str, arg: &dyn PointArg) -> Result<Box<dyn PointRunner>, Error>{
@@ -22,17 +22,17 @@ pub async fn create_kind_runner(kind: &str, arg: &dyn PointArg) -> Result<Box<dy
     let value = match kind.trim() {
         "sleep" => sleep::create(arg).await,
 
-        #[cfg(feature = "pt_jsonapi")]
+        #[cfg(feature = "point_jsonapi")]
         "jsonapi" => jsonapi::create(arg).await,
-        #[cfg(feature = "pt_md5")]
+        #[cfg(feature = "point_md5")]
         "md5" => md5::create(arg).await,
-        #[cfg(feature = "pt_dubbo")]
+        #[cfg(feature = "point_dubbo")]
         "dubbo" => dubbo::create(arg).await,
-        #[cfg(feature = "pt_mysql")]
+        #[cfg(feature = "point_mysql")]
         "mysql" => mysql::create(arg).await,
-        #[cfg(feature = "pt_redis")]
+        #[cfg(feature = "point_redis")]
         "redis" => redis::create(arg).await,
-        #[cfg(feature = "pt_mongodb")]
+        #[cfg(feature = "point_mongodb")]
         "mongodb" => mongodb::create(arg).await,
         _ => rerr!("002", format!("unsupported point kind {}", kind).as_str())
     };

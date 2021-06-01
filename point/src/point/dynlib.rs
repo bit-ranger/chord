1673b1c63d9be8fd18a060e82194a78f0de12443
bit-ranger <sincerebravefight@gmail.com>
2021-06-01 13:13:27 +0800

    feat(point): dynlib

--- point/src/point/dynlib.rs
+++ point/src/point/dynlib.rs
@@ -1,17 +1,34 @@
-use async_std::future::Future;
-use async_std::pin::Pin;
 use chord_common::err;
 use chord_common::error::Error;
 use chord_common::point::{async_trait, PointArg, PointRunner, PointValue};
+use libloading::Library;
 
 struct Dynlib {
-    runner: Box<dyn PointRunner>,
+    lib: Library,
 }
 
 #[async_trait]
 impl PointRunner for Dynlib {
     async fn run(&self, arg: &dyn PointArg) -> PointValue {
-        self.runner.run(arg).await
+        let args_raw = arg.config()["args"]
+            .as_array()
+            .ok_or(err!("010", "missing args"))?;
+
+        let mut args_rendered = vec![];
+
+        for ar in args_raw {
+            let x = arg.render(ar.as_str().ok_or(err!("010", "arg must be string"))?)?;
+            args_rendered.push(x);
+        }
+
+        let args_dynlib: Vec<&str>= args_rendered.iter().map(|a| a.as_str()).collect();
+
+        let dynlib_run: libloading::Symbol<
+            fn(
+                Vec<&str>
+            ) ->  PointValue
+        > = unsafe { self.lib.get(b"run")? };
+        dynlib_run(args_dynlib)
     }
 }
 
@@ -19,15 +36,10 @@ pub async fn create(arg: &dyn PointArg) -> Result<Box<dyn PointRunner>, Error> {
     let path = arg.config()["path"]
         .as_str()
         .ok_or(err!("010", "missing path"))?;
-    println!("loading dynlib {}", path);
+
     let lib = unsafe { libloading::Library::new(path)? };
-    let point_runner_create: libloading::Symbol<
-        fn(
-            &dyn PointArg,
-        ) -> Pin<Box<dyn Future<Output = Result<Box<dyn PointRunner>, Error>> + Send>>,
-    > = unsafe { lib.get(b"create")? };
-    let point_runner = point_runner_create(arg).await?;
+
     Ok(Box::new(Dynlib {
-        runner: point_runner,
+        lib
     }))
 }
--- zero/point-dyn/src/lib.rs
+++ zero/point-dyn/src/lib.rs
@@ -1,17 +1,7 @@
-use chord_common::error::Error;
-use chord_common::point::{async_trait, PointArg, PointRunner, PointValue};
+use chord_common::point::{PointValue};
 use chord_common::value::Json;
 
 #[no_mangle]
-pub async fn create(arg: &dyn PointArg) -> Result<Box<dyn PointRunner>, Error> {
-    Ok(Box::new(PointDyn {}))
-}
-
-struct PointDyn {}
-
-#[async_trait]
-impl PointRunner for PointDyn {
-    async fn run(&self, arg: &dyn PointArg) -> PointValue {
-        Ok(Json::String("hello point dyn".to_owned()))
-    }
+pub fn create(args: Vec<&str>) -> PointValue {
+    Ok(Json::String(format!("dynlib run: {:?}", args)))
 }

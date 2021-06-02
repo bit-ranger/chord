1f571cee703f1be6b83f7871abeaf87d9c39e1fa
bit-ranger <sincerebravefight@gmail.com>
2021-06-02 22:03:06 +0800

    feat(dynlib): run

--- zero/point-dyn/src/lib.rs
+++ zero/point-dyn/src/lib.rs
@@ -2,6 +2,6 @@ use chord_common::point::{PointValue};
 use chord_common::value::Json;
 
 #[no_mangle]
-pub fn create(args: Vec<&str>) -> PointValue {
+pub fn run(args: Vec<&str>) -> PointValue {
     Ok(Json::String(format!("dynlib run: {:?}", args)))
 }

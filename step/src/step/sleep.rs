3240cff8a7e7ba50779be7403cc7c2a61505923f
bit-ranger <sincerebravefight@gmail.com>
2021-06-04 22:21:09 +0800

    feat(step): sleep duration

--- step/src/step/sleep.rs
+++ step/src/step/sleep.rs
@@ -1,6 +1,6 @@
 use async_std::task::sleep;
 use chord_common::error::Error;
-use chord_common::step::{async_trait, RunArg, StepRunner, StepValue, CreateArg};
+use chord_common::step::{async_trait, CreateArg, RunArg, StepRunner, StepValue};
 use chord_common::value::Json;
 use std::time::Duration;
 
@@ -18,7 +18,7 @@ pub async fn create(_: Option<&Json>, _: &dyn CreateArg) -> Result<Box<dyn StepR
 }
 
 async fn run(arg: &dyn RunArg) -> StepValue {
-    let seconds = arg.config()["seconds"].as_i64().unwrap_or(0) as u64;
+    let seconds = arg.config()["duration"].as_u64().unwrap_or(0) as u64;
     sleep(Duration::from_secs(seconds)).await;
     return Ok(Json::Null);
 }

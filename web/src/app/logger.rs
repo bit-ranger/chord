d1bd685cabc428bb1e1c438704b7a5b867d5c5da
bit-ranger <sincerebravefight@gmail.com>
2021-06-05 22:34:17 +0800

    feat(task): web turn off console log

--- web/src/app/logger.rs
+++ web/src/app/logger.rs
@@ -121,8 +121,6 @@ async fn loop_write(
 
         let data = recv.unwrap();
 
-        println!("{}", String::from_utf8_lossy(&data));
-
         let _ = default_log_writer.write_all(&data).await;
     }
 }

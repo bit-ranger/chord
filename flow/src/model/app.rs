600185ef1ba41f5f3bc1087a7eeffc08d868a713
bit-ranger <sincerebravefight@gmail.com>
2021-06-11 20:00:05 +0800

    feat(helper): string

--- flow/src/model/app.rs
+++ flow/src/model/app.rs
@@ -6,7 +6,7 @@ use chord::step::StepRunnerFactory;
 
 use crate::model::helper::boolean::{ALL_HELPER, ANY_HELPER, BOOL_HELPER};
 use crate::model::helper::number::NUM_HELPER;
-use crate::model::helper::string::JOIN_HELPER;
+use crate::model::helper::string::{contains, end_with, start_with, STR_HELPER, STR_SUB_HELPER};
 
 pub trait Context: Sync + Send {
     fn get_handlebars(&self) -> &Handlebars;
@@ -24,11 +24,15 @@ impl<'reg> FlowContextStruct<'reg> {
         let mut handlebars = Handlebars::new();
 
         //C:/Users/bitranger/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/handlebars-3.5.4/src/registry.rs:118
-        handlebars.register_helper("num", Box::new(NUM_HELPER));
-        handlebars.register_helper("bool", Box::new(BOOL_HELPER));
         handlebars.register_helper("all", Box::new(ALL_HELPER));
         handlebars.register_helper("any", Box::new(ANY_HELPER));
-        handlebars.register_helper("join", Box::new(JOIN_HELPER));
+        handlebars.register_helper("num", Box::new(NUM_HELPER));
+        handlebars.register_helper("bool", Box::new(BOOL_HELPER));
+        handlebars.register_helper("str", Box::new(STR_HELPER));
+        handlebars.register_helper("contains", Box::new(contains));
+        handlebars.register_helper("start_with", Box::new(start_with));
+        handlebars.register_helper("end_with", Box::new(end_with));
+        handlebars.register_helper("str_sub", Box::new(STR_SUB_HELPER));
 
         FlowContextStruct {
             handlebars,
--- flow/src/model/helper/string.rs
+++ flow/src/model/helper/string.rs
@@ -1,14 +1,48 @@
-use handlebars::{Context, Handlebars, Helper, HelperDef, RenderContext, RenderError, ScopedJson};
-use itertools::Itertools;
-
 use chord::value::Value;
+use handlebars::handlebars_helper;
+use handlebars::{Context, Handlebars, Helper, HelperDef, RenderContext, RenderError, ScopedJson};
 
-pub static JOIN_HELPER: JoinHelper = JoinHelper {};
+handlebars_helper!(start_with: |x: Json, y: Json|
+                x.is_string() && y.is_string() && x.as_str().unwrap().starts_with(y.as_str().unwrap())
+);
+
+handlebars_helper!(end_with: |x: Json, y: Json|
+                x.is_string() && y.is_string() && x.as_str().unwrap().ends_with(y.as_str().unwrap())
+);
+
+handlebars_helper!(contains: |x: Json, y: Json|
+                x.is_string() && y.is_string() && x.as_str().unwrap().contains(y.as_str().unwrap())
+);
+
+pub static STR_HELPER: StrHelper = StrHelper {};
 
 #[derive(Clone, Copy)]
-pub struct JoinHelper;
+pub struct StrHelper;
 
-impl HelperDef for JoinHelper {
+impl HelperDef for StrHelper {
+    fn call_inner<'reg: 'rc, 'rc>(
+        &self,
+        h: &Helper<'reg, 'rc>,
+        _: &'reg Handlebars<'reg>,
+        _: &'rc Context,
+        _: &mut RenderContext<'reg, 'rc>,
+    ) -> Result<Option<ScopedJson<'reg, 'rc>>, RenderError> {
+        let param = h
+            .param(0)
+            .ok_or_else(|| RenderError::new("Param not found for helper \"str\""))?;
+
+        let param = param.value();
+
+        Ok(Some(ScopedJson::Derived(Value::String(param.to_string()))))
+    }
+}
+
+pub static STR_SUB_HELPER: StrSubHelper = StrSubHelper {};
+
+#[derive(Clone, Copy)]
+pub struct StrSubHelper;
+
+impl HelperDef for StrSubHelper {
     fn call_inner<'reg: 'rc, 'rc>(
         &self,
         h: &Helper<'reg, 'rc>,
@@ -17,17 +51,36 @@ impl HelperDef for JoinHelper {
         _: &mut RenderContext<'reg, 'rc>,
     ) -> Result<Option<ScopedJson<'reg, 'rc>>, RenderError> {
         let param = h.params();
-        if param.len() == 0 {
-            return Err(RenderError::new("Param not found for helper \"join\""));
+        let str = param[0]
+            .value()
+            .as_str()
+            .ok_or(RenderError::new("Param invalid for helper \"str_sub\""))?;
+
+        if param.len() == 2 {
+            let start = param[1]
+                .value()
+                .as_f64()
+                .ok_or(RenderError::new("Param invalid for helper \"str_sub\""))?
+                as usize;
+            return Ok(Some(ScopedJson::Derived(Value::String(
+                str[start..].to_owned(),
+            ))));
+        } else if param.len() == 3 {
+            let start = param[1]
+                .value()
+                .as_f64()
+                .ok_or(RenderError::new("Param invalid for helper \"str_sub\""))?
+                as usize;
+            let end = param[2]
+                .value()
+                .as_f64()
+                .ok_or(RenderError::new("Param invalid for helper \"str_sub\""))?
+                as usize;
+            return Ok(Some(ScopedJson::Derived(Value::String(
+                str[start..end].to_owned(),
+            ))));
+        } else {
+            return Err(RenderError::new("Param invalid for helper \"str_sub\""));
         }
-
-        let sep = param[0].value().to_string();
-
-        let str_join = param[1..]
-            .iter()
-            .map(|s| s.value().to_string())
-            .join(sep.as_str());
-
-        return Ok(Some(ScopedJson::Derived(Value::String(str_join))));
     }
 }

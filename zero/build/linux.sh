fdda263740490086b16a18622400db5f63756cc0
bit-ranger <sincerebravefight@gmail.com>
2021-06-18 13:56:43 +0800

    build(cross): crsoss build

--- zero/build/linux.sh
+++ zero/build/linux.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # linux
-docker run --rm -u "$(id -u)":"$(id -g)" -v "$(pwd)":/workdir multiarch/crossbuild /usr/local/rust/bin/cargo build --release --verbose
+docker run --rm -u "$(id -u)":"$(id -g)" -v "$(pwd)":/workdir rust-crossbuild  /usr/local/rust/bin/cargo build --release --verbose

0838c0d6e27190f5a3618d92a110c75f1e971fd2
bit-ranger <sincerebravefight@gmail.com>
2021-06-25 22:29:44 +0800

    feat(action): docker

--- action/src/action/docker.rs
+++ action/src/action/docker.rs
@@ -1,8 +1,8 @@
 use async_std::prelude::*;
 use chord::step::{async_trait, Action, ActionFactory, ActionValue, CreateArg, RunArg};
-use chord::value::{json, Value};
+use chord::value::{from_str, json, Value};
 use chord::Error;
-use chord::{err, rcause, rerr};
+use chord::{cause, err, rcause, rerr};
 use log::trace;
 use std::str::FromStr;
 use surf::http::headers::{HeaderName, HeaderValue};
@@ -49,6 +49,7 @@ async fn create0(_: &Factory, arg: &dyn CreateArg) -> Result<Box<dyn Action>, Ra
         format!("images/create?fromImage={}", image).as_str(),
         Method::Post,
         None,
+        1,
     )
     .await?;
     Ok(Box::new(Runner { image }))
@@ -60,22 +61,27 @@ async fn run0(runner: &Runner, arg: &dyn RunArg) -> Result<Value, Rae> {
         .render_value(&arg.config()["tail"])?
         .as_u64()
         .unwrap_or(1) as usize;
+
     call(
         format!("containers/create?name={}", arg.id()).as_str(),
         Method::Post,
         Some(json!({ "Image": runner.image.as_str(),
                             "Cmd": cmd
         })),
+        1,
     )
     .await?;
     trace!("create {}", arg.id());
+
     call(
         format!("containers/{}/start", arg.id()).as_str(),
         Method::Post,
         None,
+        1,
     )
     .await?;
     trace!("start {}", arg.id());
+
     let log_content = call(
         format!(
             "containers/{}/logs?stdout=true&stderr=true&tail={}",
@@ -85,20 +91,29 @@ async fn run0(runner: &Runner, arg: &dyn RunArg) -> Result<Value, Rae> {
         .as_str(),
         Method::Get,
         None,
+        tail,
     )
     .await?;
-    trace!("log {}, {}", arg.id(), log_content);
+    trace!("log {}", arg.id());
+
     call(
         format!("containers/{}?force=true", arg.id()).as_str(),
         Method::Delete,
         None,
+        1,
     )
     .await?;
     trace!("remove {}", arg.id());
-    Ok(Value::Null)
+
+    Ok(from_str(log_content.join("").as_str())?)
 }
 
-async fn call(uri: &str, method: Method, data: Option<Value>) -> Result<String, Rae> {
+async fn call(
+    uri: &str,
+    method: Method,
+    data: Option<Value>,
+    tail_size: usize,
+) -> Result<Vec<String>, Rae> {
     let url = format!("http://localhost:2375/{}", uri);
     let url = Url::from_str(url.as_str()).or(rerr!("docker", format!("invalid url: {}", url)))?;
     let mut rb = RequestBuilder::new(method, url);
@@ -112,6 +127,7 @@ async fn call(uri: &str, method: Method, data: Option<Value>) -> Result<String, 
 
     let mut res: Response = rb.send().await?;
 
+    let mut tail: Vec<String> = vec![];
     let mut line = String::new();
     loop {
         line.clear();
@@ -120,7 +136,17 @@ async fn call(uri: &str, method: Method, data: Option<Value>) -> Result<String, 
             .await
             .or_else(|e| rcause!("docker", "read fail", e))?;
         if size > 0 {
+            if res.content_type().is_some()
+                && res.content_type().unwrap().to_string() == "application/octet-stream"
+            {
+                line = String::from_utf8_lossy(&line.as_bytes()[8..]).to_string();
+            }
+
             trace!("{}", line.trim_end());
+            if tail.len() >= tail_size {
+                tail.pop();
+            }
+            tail.push(line.clone());
         } else {
             break;
         }
@@ -128,7 +154,7 @@ async fn call(uri: &str, method: Method, data: Option<Value>) -> Result<String, 
     return if !res.status().is_success() {
         rerr!("docker", res.status().to_string())?
     } else {
-        Ok(line)
+        Ok(tail)
     };
 }
 
@@ -143,5 +169,11 @@ impl From<surf::Error> for Rae {
 impl From<chord::Error> for Rae {
     fn from(err: Error) -> Self {
         Rae(err)
+    }
+}
+
+impl From<chord::value::Error> for Rae {
+    fn from(err: chord::value::Error) -> Self {
+        Rae(cause!("docker", "parse fail", err))
     }
 }

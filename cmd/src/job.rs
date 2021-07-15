2ae35983e88f02fad7f24dd348eb00439f2f849c
bit-ranger <sincerebravefight@gmail.com>
2021-07-15 15:22:21 +0800

    feat(output): enable choose report

--- cmd/src/job.rs
+++ cmd/src/job.rs
@@ -15,16 +15,16 @@ use chord_flow::{Context, TaskIdSimple};
 use chord_output::report::{Factory, ReportFactory};
 
 pub async fn run<P: AsRef<Path>>(
-    input_dir: P,
+    job_path: P,
     task_vec: Option<Vec<String>>,
     exec_id: String,
     app_ctx: Arc<dyn Context>,
     conf: &Config,
 ) -> Result<Vec<TaskState>, Error> {
-    let job_path_str = input_dir.as_ref().to_str().unwrap();
+    let job_path_str = job_path.as_ref().to_str().unwrap();
 
     trace!("job start {}", job_path_str);
-    let mut job_dir = read_dir(input_dir.as_ref()).await.unwrap();
+    let mut job_dir = read_dir(job_path.as_ref()).await.unwrap();
 
     let report_factory = ReportFactory::new(conf.report(), "chord_cmd").await?;
     let report_factory = Arc::new(report_factory);
@@ -56,9 +56,9 @@ pub async fn run<P: AsRef<Path>>(
 
         let builder = Builder::new().name(task_name);
 
-        let task_input_dir = input_dir.as_ref().join(task_dir.path());
+        let task_input_dir = job_path.as_ref().join(task_dir.path());
         let jh = builder
-            .spawn(run_task(
+            .spawn(task_run(
                 task_input_dir,
                 exec_id.clone(),
                 app_ctx.clone(),
@@ -73,46 +73,43 @@ pub async fn run<P: AsRef<Path>>(
     return Ok(task_state_vec);
 }
 
-async fn run_task<P: AsRef<Path>>(
-    input_dir: P,
+async fn task_run<P: AsRef<Path>>(
+    task_path: P,
     exec_id: String,
     app_ctx: Arc<dyn Context>,
     report_factory: Arc<ReportFactory>,
 ) -> TaskState {
-    let input_dir = Path::new(input_dir.as_ref());
-    let rt = run_task0(input_dir, exec_id, app_ctx, report_factory).await;
-    match rt {
-        Ok(ts) => {
-            trace!("task end {}", input_dir.to_str().unwrap());
-            ts
-        }
-        Err(e) => {
-            info!("task error {}, {}", input_dir.to_str().unwrap(), e);
-            TaskState::Err(e)
-        }
-    }
+    let task_path = Path::new(task_path.as_ref());
+    trace!("task start {}", task_path.to_str().unwrap());
+    let task_state = task_run0(task_path, exec_id, app_ctx, report_factory).await;
+    return if let Err(e) = task_state {
+        info!("task error {}, {}", task_path.to_str().unwrap(), e);
+        TaskState::Err(e.clone())
+    } else {
+        trace!("task end {}", task_path.to_str().unwrap());
+        task_state.unwrap()
+    };
 }
 
-async fn run_task0<I: AsRef<Path>>(
-    input_dir: I,
+async fn task_run0<P: AsRef<Path>>(
+    task_path: P,
     exec_id: String,
     app_ctx: Arc<dyn Context>,
     report_factory: Arc<ReportFactory>,
 ) -> Result<TaskState, Error> {
-    let input_dir = Path::new(input_dir.as_ref());
-    let task_id = input_dir.file_name().unwrap().to_str().unwrap();
+    let task_path = Path::new(task_path.as_ref());
+    let task_id = task_path.file_name().unwrap().to_str().unwrap();
 
     let task_id = Arc::new(TaskIdSimple::new(exec_id, task_id.to_owned())?);
     chord_flow::CTX_ID.with(|tid| tid.replace(task_id.to_string()));
+    trace!("task start {}", task_path.to_str().unwrap());
 
-    trace!("task start {}", input_dir.to_str().unwrap());
-
-    let flow_file = input_dir.clone().join("flow.yml");
+    let flow_file = task_path.clone().join("flow.yml");
     let flow = chord_input::load::flow::yml::load(&flow_file)?;
     let flow = Flow::new(flow)?;
 
     //read
-    let data_file_path = input_dir.clone().join("case.csv");
+    let data_file_path = task_path.clone().join("case.csv");
     let data_loader = Box::new(chord_input::load::data::csv::Loader::new(data_file_path).await?);
 
     //write

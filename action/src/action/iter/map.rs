e135cfc9904aa2ad65a675ac9fd22a197a0b7110
bit-ranger <sincerebravefight@gmail.com>
2021-07-31 14:06:04 +0800

    feat(flow): refactor flow for iter_map

--- action/src/action/iter/map.rs
+++ action/src/action/iter/map.rs
@@ -1,6 +1,7 @@
 use async_std::sync::Arc;
 use chord::action::prelude::*;
 use chord::action::{CreateId, RunId};
+use itertools::Itertools;
 use std::collections::HashMap;
 use std::time::Duration;
 
@@ -17,11 +18,11 @@ impl IterMapFactory {
     }
 }
 
-struct MapperCreateArg<'a> {
+struct MapCreateArg<'a> {
     iter_arg: &'a dyn CreateArg,
 }
 
-impl<'a> CreateArg for MapperCreateArg<'a> {
+impl<'a> CreateArg for MapCreateArg<'a> {
     fn id(&self) -> &dyn CreateId {
         self.iter_arg.id()
     }
@@ -30,8 +31,8 @@ impl<'a> CreateArg for MapperCreateArg<'a> {
         self.iter_arg.action()
     }
 
-    fn args(&self) -> &Value {
-        &self.iter_arg.args()["map"]["args"]
+    fn args_raw(&self) -> &Value {
+        &self.iter_arg.args_raw()["map"]["args"]
     }
 
     fn render_str(&self, text: &str) -> Result<String, Error> {
@@ -44,20 +45,30 @@ impl<'a> CreateArg for MapperCreateArg<'a> {
 }
 
 struct IterMap {
-    item_action: Box<dyn Action>,
+    action: Box<dyn Action>,
 }
 
-struct MapperRunArg<'a> {
+struct MapRunArg<'a, 'i> {
     iter_arg: &'a dyn RunArg,
+    index: usize,
+    index_name: String,
+    item: &'i Value,
+    item_name: String,
 }
 
-impl<'a> RunArg for MapperRunArg<'a> {
+impl<'a, 'i> RunArg for MapRunArg<'a, 'i> {
     fn id(&self) -> &dyn RunId {
         self.iter_arg.id()
     }
 
-    fn args(&self) -> &Value {
-        &self.iter_arg.args()["map"]["args"]
+    fn args(&self, ctx: Option<Map>) -> Result<Value, Error> {
+        let mut ctx = ctx.unwrap_or(Map::new());
+        ctx.insert(
+            self.index_name.clone(),
+            Value::Number(Number::from(self.index)),
+        );
+        ctx.insert(self.item_name.clone(), self.item.clone());
+        Ok(self.iter_arg.args(Some(ctx))?["map"]["args"].clone())
     }
 
     fn timeout(&self) -> Duration {
@@ -68,30 +79,45 @@ impl<'a> RunArg for MapperRunArg<'a> {
 #[async_trait]
 impl Factory for IterMapFactory {
     async fn create(&self, arg: &dyn CreateArg) -> Result<Box<dyn Action>, Error> {
-        let action = arg.args()["map"]["action"]
+        let action = arg.args_raw()["map"]["action"]
             .as_str()
             .ok_or(err!("101", "missing action"))?;
         let factory = self
             .table
             .get(action)
             .ok_or(err!("102", "unsupported action"))?;
-        let item_create_arg = MapperCreateArg { iter_arg: arg };
+        let map_create_arg = MapCreateArg { iter_arg: arg };
 
-        let item_action = factory.create(&item_create_arg).await?;
-        Ok(Box::new(IterMap { item_action }))
+        let map_action = factory.create(&map_create_arg).await?;
+        Ok(Box::new(IterMap { action: map_action }))
     }
 }
 
 #[async_trait]
 impl Action for IterMap {
     async fn run(&self, arg: &dyn RunArg) -> Result<Box<dyn Scope>, Error> {
-        let iter = arg.args()["iter"]
+        let args = arg.args(None)?;
+        let array = args["iter"]["arr"]
             .as_array()
-            .ok_or(err!("103", "missing iter"))?;
-        let mut iter_val = Vec::with_capacity(iter.len());
-        for item in iter {
-            let mra = MapperRunArg { iter_arg: arg };
-            let val = self.item_action.run(&mra).await?;
+            .ok_or(err!("103", "missing iter.arr"))?;
+        let enumerate = args["iter"]["enum"]
+            .as_str()
+            .ok_or(err!("104", "missing iter.enum"))?;
+        let (index_name, item_name) = enumerate
+            .split(",")
+            .collect_tuple()
+            .ok_or(err!("105", "invalid iter.enum"))?;
+
+        let mut iter_val = Vec::with_capacity(array.len());
+        for (index, item) in array.iter().enumerate() {
+            let mra = MapRunArg {
+                iter_arg: arg,
+                index,
+                index_name: index_name.to_string(),
+                item,
+                item_name: item_name.to_string(),
+            };
+            let val = self.action.run(&mra).await?;
             iter_val.push(val.as_value().clone());
         }
         Ok(Box::new(Value::Array(iter_val)))

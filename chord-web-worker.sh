0e560b3db1f03772ef3560a03ca99ac9e1739d0d
bit-ranger <sincerebravefight@gmail.com>
2021-08-13 10:29:11 +0800

    refactor(web): docker worker

--- chord-web-worker.sh
+++ chord-web-worker.sh
@@ -1,5 +1,5 @@
 ssh-add /data/chord/conf/ssh_key.pri
 git clone "$(chord.git_url)" job_repo
-cd job_repo
+cd job_repo || exit
 git checkout "$(chord.git_branch)"
-chord-cmd -i.chord/job
+chord-cmd -i"$PWD"/.chord/job

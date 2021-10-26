49f7c31c77fa8ba2a773259ebabac2c30fcb626e
bit-ranger <sincerebravefight@gmail.com>
2021-10-26 13:47:06 +0800

    feat(action): program scope

--- action/src/action/program.rs
+++ action/src/action/program.rs
@@ -1,4 +1,4 @@
-use async_std::process::Command;
+use async_std::process::{Child, Command};
 use chord::action::prelude::*;
 use log::trace;
 
@@ -12,36 +12,30 @@ impl ProgramFactory {
 
 #[async_trait]
 impl Factory for ProgramFactory {
-    async fn create(&self, _: &dyn CreateArg) -> Result<Box<dyn Action>, Error> {
-        Ok(Box::new(Program {}))
+    async fn create(&self, arg: &dyn CreateArg) -> Result<Box<dyn Action>, Error> {
+        let args_raw = Value::Object(arg.args_raw().clone());
+        match args_raw["scope"].as_str().unwrap_or("step") {
+            "task" => Ok(Box::new(TaskProgram::new(&args_raw)?)),
+            "case" => Ok(Box::new(CaseProgram::new(&args_raw)?)),
+            _ => Ok(Box::new(StepProgram::new(&args_raw)?)),
+        }
     }
 }
 
-struct Program {}
+struct StepProgram {}
+
+impl StepProgram {
+    fn new(_: &Value) -> Result<StepProgram, Error> {
+        Ok(StepProgram {})
+    }
+}
 
 #[async_trait]
-impl Action for Program {
+impl Action for StepProgram {
     async fn run(&self, arg: &dyn RunArg) -> Result<Box<dyn Scope>, Error> {
         let args = Value::Object(arg.args()?);
-
-        let cmd = args["program"]
-            .as_str()
-            .ok_or(err!("103", "missing program"))?;
-
-        let mut command = Command::new(cmd);
-
-        let cmd_args = args["args"].as_array().ok_or(err!("103", "missing args"))?;
-        for ca in cmd_args {
-            let ca = if ca.is_string() {
-                ca.as_str().unwrap().to_owned()
-            } else {
-                ca.to_string()
-            };
-            command.arg(ca);
-        }
-
+        let mut command = program_command(&args)?;
         trace!("command {:?}", command);
-
         let output = command.output().await?;
 
         let std_out = String::from_utf8_lossy(&output.stdout).to_string();
@@ -72,4 +66,98 @@ impl Action for Program {
             }
         }
     }
+}
+
+struct CaseProgram {}
+
+impl CaseProgram {
+    fn new(_: &Value) -> Result<CaseProgram, Error> {
+        Ok(CaseProgram {})
+    }
+}
+
+#[async_trait]
+impl Action for CaseProgram {
+    async fn run(&self, arg: &dyn RunArg) -> Result<Box<dyn Scope>, Error> {
+        let args = Value::Object(arg.args()?);
+
+        let mut command = program_command(&args)?;
+        trace!("command {:?}", command);
+        let child = command.spawn()?;
+        trace!("pid {:?}", child.id());
+        Ok(Box::new(ChildHolder::new(child)))
+    }
+}
+
+struct ChildHolder {
+    value: Value,
+    child: Child,
+}
+
+impl ChildHolder {
+    fn new(child: Child) -> ChildHolder {
+        ChildHolder {
+            value: Value::Number(Number::from(child.id())),
+            child,
+        }
+    }
+}
+
+impl Scope for ChildHolder {
+    fn as_value(&self) -> &Value {
+        &self.value
+    }
+}
+
+impl Drop for ChildHolder {
+    fn drop(&mut self) {
+        let _ = self.child.kill();
+    }
+}
+
+struct TaskProgram {
+    child: Child,
+}
+
+impl TaskProgram {
+    fn new(args_raw: &Value) -> Result<TaskProgram, Error> {
+        let mut command = program_command(args_raw)?;
+        trace!("command {:?}", command);
+        let child = command.spawn()?;
+        trace!("pid {:?}", child.id());
+        Ok(TaskProgram { child })
+    }
+}
+
+#[async_trait]
+impl Action for TaskProgram {
+    async fn run(&self, _: &dyn RunArg) -> Result<Box<dyn Scope>, Error> {
+        Ok(Box::new(Value::Null))
+    }
+}
+
+impl Drop for TaskProgram {
+    fn drop(&mut self) {
+        let _ = self.child.kill();
+    }
+}
+
+fn program_command(args: &Value) -> Result<Command, Error> {
+    let cmd = args["program"]
+        .as_str()
+        .ok_or(err!("103", "missing program"))?;
+
+    let mut command = Command::new(cmd);
+
+    let cmd_args = args["args"].as_array().ok_or(err!("103", "missing args"))?;
+
+    for ca in cmd_args {
+        let ca = if ca.is_string() {
+            ca.as_str().unwrap().to_owned()
+        } else {
+            ca.to_string()
+        };
+        command.arg(ca);
+    }
+    Ok(command)
 }

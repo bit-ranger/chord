259e5ea3fc2647e846750f6713c8d08030fbe502
bit-ranger <sincerebravefight@gmail.com>
2021-10-28 19:51:22 +0800

    feat(flow): meta file_dir

--- flow/src/flow/case/arg.rs
+++ flow/src/flow/case/arg.rs
@@ -93,6 +93,7 @@ impl CaseArgStruct {
         let id = Arc::new(CaseIdStruct::new(task_id, stage_id, case_exec_id, case_id));
 
         let mut render_data: Map = Map::new();
+        render_data.insert(String::from("meta"), to_value(flow.meta()).unwrap());
         if let Some(def) = flow.def() {
             render_data.insert(String::from("def"), to_value(def).unwrap());
         }

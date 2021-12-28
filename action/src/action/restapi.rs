a0a08cdae10a61c1df4869d57c8543bd6f6bb0a4
bit-ranger <sincerebravefight@gmail.com>
2021-12-28 20:23:55 +0800

    feat(action): restapi use reqwest

--- action/src/action/restapi.rs
+++ action/src/action/restapi.rs
@@ -2,13 +2,11 @@ use std::borrow::Borrow;
 use std::fmt::{Display, Formatter};
 use std::str::FromStr;
 
-use surf::http::headers::{HeaderName, HeaderValue};
-use surf::http::Method;
-use surf::{Body, RequestBuilder, Response, Url};
-
 use chord_core::action::prelude::*;
 
 use crate::err;
+use reqwest::header::{HeaderName, HeaderValue};
+use reqwest::{Body, Client, Method, Response, Url};
 
 pub struct RestapiFactory {}
 
@@ -78,6 +76,7 @@ async fn run0(arg: &dyn RunArg) -> std::result::Result<Value, Error> {
     let args = arg.args()?;
 
     let url = args["url"].as_str().ok_or(err!("100", "missing url"))?;
+
     let url = Url::from_str(url).or(Err(err!("101", format!("invalid url: {}", url))))?;
 
     let method = args["method"]
@@ -85,7 +84,8 @@ async fn run0(arg: &dyn RunArg) -> std::result::Result<Value, Error> {
         .ok_or(err!("102", "missing method"))?;
     let method = Method::from_str(method).or(Err(err!("103", "invalid method")))?;
 
-    let mut rb = RequestBuilder::new(method, url);
+    let client = Client::new();
+    let mut rb = client.request(method, url);
     rb = rb.header(
         HeaderName::from_str("Content-Type").unwrap(),
         HeaderValue::from_str("application/json; charset=utf-8")?,
@@ -93,32 +93,30 @@ async fn run0(arg: &dyn RunArg) -> std::result::Result<Value, Error> {
 
     if let Some(header) = args["header"].as_object() {
         for (k, v) in header.iter() {
-            let hn =
-                HeaderName::from_string(k.clone()).or(Err(err!("104", "invalid header name")))?;
-            let hvs: Vec<HeaderValue> = match v {
+            let hn = HeaderName::from_str(k).or(Err(err!("104", "invalid header name")))?;
+            match v {
                 Value::String(v) => {
-                    vec![HeaderValue::from_str(v).or(Err(err!("105", "invalid header value")))?]
+                    let hv =
+                        HeaderValue::from_str(v).or(Err(err!("105", "invalid header value")))?;
+                    rb = rb.header(hn, hv);
                 }
                 Value::Array(vs) => {
-                    let mut vec = vec![];
                     for v in vs {
-                        let v = HeaderValue::from_str(v.to_string().as_str())?;
-                        vec.push(v)
+                        let hv = HeaderValue::from_str(v.to_string().as_str())?;
+                        rb = rb.header(hn.clone(), hv);
                     }
-                    vec
                 }
                 _ => Err(err!("106", "invalid header value"))?,
             };
-            rb = rb.header(hn, hvs.as_slice());
         }
     }
 
     let body = args["body"].borrow();
     if !body.is_null() {
-        rb = rb.body(Body::from(body.clone()));
+        rb = rb.body(Body::from(body.to_string()));
     }
 
-    let mut res: Response = rb.send().await?;
+    let res: Response = rb.send().await?;
     let mut res_data = Map::new();
     res_data.insert(
         String::from("status"),
@@ -126,15 +124,12 @@ async fn run0(arg: &dyn RunArg) -> std::result::Result<Value, Error> {
     );
 
     let mut header_data = Map::new();
-    for (hn, hv) in res.iter() {
-        header_data.insert(
-            hn.to_string(),
-            Value::Array(hv.iter().map(|v| Value::String(v.to_string())).collect()),
-        );
+    for (hn, hv) in res.headers() {
+        header_data.insert(hn.to_string(), Value::String(hv.to_str()?.to_string()));
     }
     res_data.insert(String::from("header"), Value::Object(header_data));
 
-    let body_str = res.body_string().await?;
+    let body_str = res.text().await?;
     if !body_str.is_empty() {
         let body = body_str.parse()?;
         res_data.insert(String::from("body"), body);

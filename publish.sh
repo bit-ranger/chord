0f479e3d82c66cf61c59fcc51944ff0a07b9bede
bit-ranger <sincerebravefight@gmail.com>
2021-12-30 23:16:23 +0800

    feat(chord): publish.sh

--- publish.sh
+++ publish.sh
@@ -2,48 +2,14 @@
 
 set -e
 
-cd core
-cargo check
-cargo publish
+entry="$(pwd)"
 
-sleep 1m
-cd ..
-cd util
-cargo check
-cargo publish
-
-sleep 1m
-cd ..
-cd action
-cargo check
-cargo publish
-
-sleep 1m
-cd ..
-cd input
-cargo check
-cargo publish
-
-sleep 1m
-cd ..
-cd output
-cargo check
-cargo publish
-
-sleep 1m
-cd ..
-cd flow
-cargo check
-cargo publish
-
-sleep 1m
-cd ..
-cd cli
-cargo check
-cargo publish
-
-sleep 1m
-cd ..
-cd web
-cargo check
-cargo publish
+for mod in "core" "action" "input" "output" "flow" "cli" "web"
+do
+  cd "$mod"
+  echo "entering $(pwd)"
+  cargo check
+  cargo publish
+  cd "$entry"
+  sleep 1m
+done

c26f75d1724a8d54221c66e0afa5818eeef3c22b
张斌 <sincerebravefight@gmail.com>
2023-02-28 21:16:20 +0800

    feat(action): quickjs single rt

--- action/src/action/quickjs.rs
+++ action/src/action/quickjs.rs
@@ -1,3 +1,4 @@
+use std::sync::Arc;
 use std::time::Duration;
 
 use futures::executor::block_on;
@@ -15,22 +16,50 @@ use chord_core::action::prelude::*;
 
 use crate::err;
 
-pub struct QuickjsCreator {}
+pub struct QuickjsCreator {
+    rt: Arc<QuickJsRuntimeFacade>,
+}
 
 impl QuickjsCreator {
     pub async fn new(_: Option<Value>) -> Result<QuickjsCreator, Error> {
-        Ok(QuickjsCreator {})
+        let rt = QuickJsRuntimeBuilder::new().js_build();
+        rt.js_loop_realm_sync(None, |_rt_adapter, realm_adapter| {
+            let proxy = JsProxy::new(&["chord", "action"], "Http").add_static_method(
+                "newClient",
+                |_rt_adapter, realm_adapter: &QuickJsRealmAdapter, args| {
+                    let args_str = format!("{}", &args[0].js_to_i32());
+                    realm_adapter.js_promise_create_resolving_async(
+                        async { Ok(args_str) },
+                        |realm_adapter, producer_result| {
+                            realm_adapter.js_string_create(producer_result.as_str())
+                        },
+                    )
+                },
+            );
+            realm_adapter
+                .js_proxy_install(proxy, true)
+                .ok()
+                .expect("could not install proxy");
+        });
+
+        Ok(QuickjsCreator {
+            rt: Arc::new(rt)
+        })
     }
 }
 
 #[async_trait]
 impl Creator for QuickjsCreator {
     async fn create(&self, _chord: &dyn Chord, _arg: &dyn Arg) -> Result<Box<dyn Action>, Error> {
-        Ok(Box::new(Quickjs {}))
+        Ok(Box::new(Quickjs {
+            rt: self.rt.clone()
+        }))
     }
 }
 
-struct Quickjs {}
+struct Quickjs {
+    rt: Arc<QuickJsRuntimeFacade>,
+}
 
 #[async_trait]
 impl Action for Quickjs {
@@ -44,42 +73,24 @@ impl Action for Quickjs {
             .as_str()
             .ok_or(err!("100", "missing quickjs"))?
             .to_string();
-        let rt = QuickJsRuntimeBuilder::new().js_build();
-        block_on(run_code(&rt, &code))?;
+
+        block_on(run_code(self.rt.as_ref(), &code))?;
         return Ok(Asset::Value(Value::Null));
     }
 }
 
-struct HttpClient {}
 
-async fn run_code(rt: &QuickJsRuntimeFacade, code: &str) -> Result<(), JsError> {
+async fn run_code(rt: &QuickJsRuntimeFacade, code: &str) -> Result<(), Error> {
 
     // create simple proxy class with an async function
-    rt.js_loop_realm_sync(None, |_rt_adapter, realm_adapter| {
-        let proxy = JsProxy::new(&["chord", "action"], "Http").add_static_method(
-            "newClient",
-            |_rt_adapter, realm_adapter: &QuickJsRealmAdapter, args| {
-                let args_str = format!("{}", &args[0].js_to_i32());
-                realm_adapter.js_promise_create_resolving_async(
-                    async { Ok(args_str) },
-                    |realm_adapter, producer_result| {
-                        realm_adapter.js_string_create(producer_result.as_str())
-                    },
-                )
-            },
-        );
-        realm_adapter
-            .js_proxy_install(proxy, true)
-            .ok()
-            .expect("could not install proxy");
-    });
 
     rt.js_eval(
         None,
         Script::new(
-            "testMyProxy.js",
+            "tmp.js",
             code,
         ),
-    ).await?;
+    ).await
+        .map_err(|e| err!("110", "could not eval code"))?;
     Ok(())
 }
